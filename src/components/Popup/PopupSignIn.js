cb6eafeb164c3b99928408103fbbe3e08517470f
Timur Ismagilov <ismagilov42@yandex.ru>
2020-11-16 14:04:14 +0300

    validation done

--- src/components/Popup/PopupSignIn.js
+++ src/components/Popup/PopupSignIn.js
@@ -5,11 +5,34 @@ class PopupSignIn extends React.Component {
     constructor(props) {
         super(props);
         this._signInCallback = props.signInCallback;
+        this._validation = this._validation.bind(this);
         this.state = {
-            isUserNameValid: false,
+            isUsernameValid: false,
             isPasswordValid: false,
             isFormValid: false
         }
+    }
+
+    _validation(event) {
+       console.log(event.target.name);
+       if(event.target.name === 'name') {
+            if(this._username.value.length === 0) {
+                this._usernameError.textContent = 'Это обязательное поле';
+                this.setState((state)=>{return {...state, isUsernameValid: false}})
+            } else {
+                this._usernameError.textContent = '';
+                this.setState((state)=>{return {...state, isUsernameValid: true}})
+            }
+       } else if(event.target.name === 'password') {
+            if(this._password.value.length === 0) {
+                this._passwordError.textContent = 'Это обязательное поле';
+                this.setState((state)=>{return {...state, isPasswordValid: false}})
+            } else {
+                this._usernameError.textContent = '';
+                this.setState((state)=>{return {...state, isPasswordValid: true}})
+            }
+       }
+       this.setState((state)=>{return {...state, isFormValid: state.isPasswordValid && state.isUsernameValid}})
     }
 
     _signIn(event) {
@@ -31,13 +54,13 @@ class PopupSignIn extends React.Component {
             <div className="popup__content">
                 <img src="/images/close.svg" alt="X" onClick={this.props.onCloseClick} className="popup__close" />
                 <h3 className="popup__title">Авторизироваться</h3>
-                <form className="popup__form" name="new" noValidate>
-                    <input type="text" name="name" className="popup__input" placeholder="Имя пользователя" ref={(input) => {this._username = input}} required />
-                    <span id="name" className="popup__error"></span>
-                    <input type="password" name="password" className="popup__input" placeholder="Пароль" ref={(input) => {this._password = input}} required />
-                    <span id="password" className="popup__error"></span>
+                <form className="popup__form" name="new">
+                    <input type="text" name="name" className="popup__input" placeholder="Имя пользователя" ref={(input) => {this._username = input}} onChange={this._validation} />
+                    <span id="name" className="popup__error" ref={(span) => {this._usernameError = span}}></span>
+                    <input type="password" name="password" className="popup__input" placeholder="Пароль" ref={(input) => {this._password = input}} onChange={this._validation} />
+                    <span id="password" className="popup__error" ref={(span) => {this._passwordError = span}}></span>
                     <span id="enter" className="popup__button-error" ref={(span) => {this._buttonError = span}}></span>
-                    <button name="enter" className="popup__button" onClick={this._signIn.bind(this)}>Войти</button>
+                    <button name="enter" className={this.state.isFormValid ? "popup__button" : "popup__button-disable"} onClick={this._signIn.bind(this)} disabled={!this.state.isFormValid}>Войти</button>
                 </form>
             </div>
         )
